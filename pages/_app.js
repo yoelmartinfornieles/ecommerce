4bc209abe31b2213eeb741f91ae30b289dc84980
Yoel Martin <yoelmartinfornieles@gmail.com>
2021-11-02 17:06:11 +0100

    feat: Fixing the SSR from materialUi

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+
+  // Fixing the SSR from materialui
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
+
   return <Component {...pageProps} />
 }
 
---
+++ pages/_document.js
@@ -0,0 +1,3 @@
+/* export default class MyDocument extends Document {
+
+} */
