f0508c53143e6243938a126472fe43a9d551c8b7
Yoel Martin <yoelmartinfornieles@gmail.com>
2021-11-17 18:49:41 +0100

    feat: data added

--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,32 @@
-/* export default class MyDocument extends Document {
+import { ServerStyleSheets } from '@material-ui/core/styles';
+import Document, { Head, Main, NextScript, Html } from 'next/document'
+import React from 'react'
 
-} */
+export default class MyDocument extends Document {
+	render () {
+		return (
+			<Html lang='en'>
+				<Head></Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		)}
+}
+
+MyDocument.getInitialProps = async ctx => {
+	const sheets = new ServerStyleSheets();
+	const originalRenderPage = ctx.renderPage;
+	ctx.renderPage = () => originalRenderPage({
+		enhanceApp: App => props => sheets.collect(<App {...props} />)
+	})
+	const initialProps = await Document.getInitialProps(ctx)
+	return {
+		...initialProps,
+		styles: [
+			...React.Children.toArray(initialProps.styles),
+			sheets.getStyleElement()
+		]
+	}
+}
---
+++ utils/data.js
@@ -0,0 +1,64 @@
+const data = {
+  products: [
+    {
+      name: 'Sandman overture',
+      images: '/public/images/sandman.jpg',
+      price: 12.95,
+      editor: 'ECC',
+      authors: 'Neil Gaiman',
+      rating: 4.1,
+      numReviews: 10,
+      countInStock: 20,
+      description:
+        'Sandman: Obertura es una novela gráfica del universo de The Sandman, escrita por Neil Gaiman y dibujada por J. H. Williams III.​ Está conformada por seis números, publicados entre octubre de 2013 y septiembre de 2015.​',
+    },
+    {
+      name: 'Preacher Deluxe I',
+      images: '/public/images/preacher.jpeg',
+      price: 33.95,
+      editor: 'ECC',
+      authors: 'Garth Ennis, Steve Dillon',
+      rating: 4.8,
+      numReviews: 12,
+      countInStock: 1,
+      description:
+        '¡La nueva edición Deluxe de Predicador! Tras fusionarse con una extraña entidad llamada Génesis, el predicador Jesse Custer obtiene el poder de "La Palabra", ¡una habilidad sobrenatural que le otorga el poder de que las personas hagan lo que él diga!​',
+    },
+    {
+      name: 'Hellblazer Ennis Vol.5',
+      images: '/public/images/hellblazer.jpeg',
+      price: 12.99,
+      editor: 'Salvat',
+      authors: 'Garth Ennis, Steve Dillon',
+      rating: 4.6,
+      numReviews: 16,
+      countInStock: 23,
+      description:
+        'La presente entrega de la colección contiene un nuevo puñado de historias publicadas originalmente en los números del 71 al 75 de Hellblazer, en ´Hellblazer Special núm. 1 USA´ y en la antología ´Vertigo Winter’s Edge núm. 2 USA´.​',
+    },
+    {
+      name: 'Planetary Omnibus',
+      images: '/public/images/planetary.jpeg',
+      price: 54.22,
+      editor: 'DCcomics',
+      authors: 'Warren Ellis, John Cassaday',
+      rating: 5.0,
+      numReviews: 20,
+      countInStock: 333,
+      description:
+        'Planetary has been hailed as a timeless story that turned modern superhero conventions on their heads.Written by Warren Ellis (Transmetropolitan) and with stunning art by John Cassaday (Astonishing X-Men), this critically acclaimed, landmark series took a look at the inter-dimensional peace-keeping force known as Planetary.',
+    },
+    {
+      name: 'Preacher Deluxe I',
+      images: '/public/images/starbrand.jpeg',
+      price: 24.99,
+      editor: 'MARVEL',
+      authors: 'Jim Shooter, John Byrne, John Romita JR.',
+      rating: 4.0,
+      numReviews: 10,
+      countInStock: 0,
+      description:
+        'There is one weapon in the galaxy more powerful than all others. Nothing is more coveted or feared, loved or hated. To dare possess it makes you a fool or madman...or, just possibly, the right man!',
+    },
+  ],
+};
